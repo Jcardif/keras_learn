8ed27f9e864fee25f3374c37f29d874c71c6f642
Josh Cardif <josh.cardif14@outlook.com>
2018-12-20 14:48:27 +0300

    Add 3

--- cifar10.py
+++ cifar10.py
@@ -1,15 +1,95 @@
+
+from __future__ import print_function
 import keras
-import tensorflow as tf
-import time
+from keras.datasets import cifar10
+from keras.models import load_model
+import os
+import numpy as np
+
 import matplotlib.pyplot as plt
-import numpy as np 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.constraints import maxnorm
-from keras.optimizers import SGD
-from keras.layers import Activation
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.normalization import BatchNormalization
+from matplotlib import cm
+%matplotlib inline
+
+import random
+
+
+'''
+LOAD CIFAR10
+'''
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+print(type(x_test))
+print(type(y_test[0]))
+
+# cifar10 category label name
+cifar10_labels = np.array([
+    'airplane',
+    'automobile',
+    'bird',
+    'cat',
+    'deer',
+    'dog',
+    'frog',
+    'horse',
+    'ship',
+    'truck'])
+
+    
+'''
+LOAD MODEL AND PREDICT
+'''
+
+# use ResNet
+model = load_model('cifar10_ResNet20v1_model.092.h5')
+
+# use CNN
+# model = load_model('keras_cifar10_trained_model.h5')
+
+    
+def convertCIFER10Data(image):
+    img = image.astype('float32')
+    img /= 255
+    c = np.zeros(32*32*3).reshape((1,32,32,3))
+    c[0] = img
+    return c
+
+plt.figure(figsize=(16,16))
+
+right = 0
+mistake = 0
+
+for i in range(100):
+    index = random.randint(0, x_test.shape[0])
+    image = x_test[index]
+    data = convertCIFER10Data(image)
+
+    plt.subplot(10, 10, i+1)
+    plt.imshow(image)
+    plt.axis('off')
+
+    ret = model.predict(data, batch_size=1) 
+    #print(ret)
+
+    bestnum = 0.0
+    bestclass = 0
+    for n in [0,1,2,3,4,5,6,7,8,9]:
+        if bestnum < ret[0][n]:
+            bestnum = ret[0][n]
+            bestclass = n
+
+    if y_test[index] == bestclass:
+        plt.title(cifar10_labels[bestclass])
+        right += 1
+    else:
+        plt.title(cifar10_labels[bestclass] + "!=" + cifar10_labels[y_test[index][0]], color='#ff0000')
+        mistake += 1
+                                                                   
+plt.show()
+print("The number of correct answers:", right)
+print("The number of mistake:", mistake)
+print("A correct answer rate:", right/(mistake + right)*100, '%')
