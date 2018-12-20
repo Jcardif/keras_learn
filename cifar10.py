12cb5a1c429a2266f66f4ed664e5f2cf77d3d2b6
Ubuntu <Josh@DataSci.4te1xpguinbu5p3031gg30tsee.bx.internal.cloudapp.net>
2018-12-20 10:11:25 +0000

    Add 2

---
+++ cifar10.py
@@ -0,0 +1,15 @@
+import keras
+import tensorflow as tf
+import time
+import matplotlib.pyplot as plt
+import numpy as np 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers import Activation
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
